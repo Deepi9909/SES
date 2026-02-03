ab8c155f9fd944a92ea30b2324133da902d06523
Deepika R <103570068+Deepi9909@users.noreply.github.com>
2026-02-03 10:46:25 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // API Base URL - loaded from environment variable
-const API_BASE_URL = 'https://fnc-vmp-weu1-d-030-fnekhxd8g7c5hdhs.westeurope-01.azurewebsites.net/api/http_trigger?code=m0l-3wu7s1G-tCl9CJ-JHf9vt0--NQe4UkWnzI0QzhaDAzFuSTx5yw==';
+const API_BASE_URL = 'https://fnc-vmp-weu1-d-030-fnekhxd8g7c5hdhs.westeurope-01.azurewebsites.net/api/http_trigger?code=cvHQ9x4n9ubdI4Jk8WSlda-xiUKMveRM4vYWXE4eq58vAzFuoXCCpg==';
 
 
 
